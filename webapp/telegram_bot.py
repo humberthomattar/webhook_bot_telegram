62d4c498fb1af70b8e39144fc63811912e2fdf2b
humberthomattar <humberthomattar@gmail.com>
2018-02-19 09:20:08 -0300

    Correcao das importacoes e uso o Utils

--- webapp/telegram_bot.py
+++ webapp/telegram_bot.py
@@ -1,22 +1,10 @@
 import telegram
-import utils
-import json
+from webapp import utils
 from webapp import message_handler
 from webapp import app
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
 
-def load_json(file):
-    """
-    :objetivo: Funcao especializada na leitura do arquivo json especificado.
-    :param file: Path do arquivo json que será lido pela aplicação.
-    :return: Dicionario com as chaves e valores do arquivo json.
-    """
-    try:
-        with open(file) as data:
-            return json.load(data)
-    except Exception as e:
-        print(str(e))
 
 def start(bot, update):
     msg = message_handler.BOT_MSG_START
@@ -26,26 +14,24 @@ def start(bot, update):
 def inscrever(bot, update):
     try:
         #valid = False
-        chats = load_json('webapp/chats.json')
+        chats = utils.load_json('webapp/chats.json')
         if update.message.chat_id not in chats['chat_id']:
             chats['chat_id'].append(update.message.chat_id)
-            with open('webapp/chats.json', 'w') as f:
-                json.dump(chats, f)
+            utils.write_json('webapp/chats.json', chats)
             bot.send_message(chat_id=update.message.chat_id, text=message_handler.BOT_MSG_JSON_INSC_OK)
         else:
             bot.send_message(chat_id=update.message.chat_id, text=message_handler.BOT_MSG_JSON_INSC_NOK)
     except Exception as e:
         print(str(e))
 
-# TODO => Verify why library's Utils attributes 'load_json' and 'write_json' doesn't work on Python 3.X
+
 def desinscrever(bot, update):
-    chats = load_json('webapp/chats.json')
+    chats = utils.load_json('webapp/chats.json')
     if update.message.chat_id not in chats['chat_id']:
         bot.send_message(chat_id=update.message.chat_id, text=message_handler.BOT_MSG_JSON_DESINSC_OK)
     else:
         chats['chat_id'].remove(update.message.chat_id)
-        with open('webapp/chats.json', 'w') as f:
-            json.dump(chats, f)
+        utils.write_json('webapp/chats.json', chats)
         bot.send_message(chat_id=update.message.chat_id, text=message_handler.BOT_MSG_JSON_DESINSC_NOK)
 
 
@@ -75,12 +61,14 @@ else:
     updater = Updater(token=app.config['TELEGRAM_TOKEN'])
 dispatcher = updater.dispatcher
 
+
 # Criacao dos objetos de comando #
 start_handler = CommandHandler('start', start)
 menu_handler = CommandHandler('menu', menu)
 inscrever_handler = CommandHandler('inscrever', inscrever)
 desinscrever_handler = CommandHandler('desinscrever', desinscrever)
 unknown_handler = MessageHandler(Filters.command, unknown)
+
 
 # ADD Comandos ao dispatcher #
 dispatcher.add_handler(start_handler)
